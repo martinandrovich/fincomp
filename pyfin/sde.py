3003e09ed785a13a1d894b2b74fb559322153cdd
Martin Androvich <martinandrovich@gmail.com>
2022-06-19 19:07:08 +0200

    add heston + fix typos

--- pyfin/sde.py
+++ pyfin/sde.py
@@ -66,7 +66,7 @@ def gbm(s0, mu, sigma, T, dt=0.01, num_paths=10, reproducible=False):
 	S[:, 0] = s0
 	X[:, 0] = np.log(s0)
 
-	for i in range(0, num_steps):
+	for i in range(num_steps):
 
 		if num_paths > 1:  # standardize samples (mean: 0, var: 1)
 			Z[:, i] = (Z[:, i] - np.mean(Z[:, i])) / np.std(Z[:, i])
@@ -104,7 +104,7 @@ def ou(s0, kappa, theta, sigma, T, dt=0.01, num_paths=10, reproducible=False):
 
 	S[:, 0] = s0
 
-	for i in range(0, num_steps):
+	for i in range(num_steps):
 
 		if num_paths > 1:  # standardize samples (mean: 0, var: 1)
 			Z[:, i] = (Z[:, i] - np.mean(Z[:, i])) / np.std(Z[:, i])
@@ -185,3 +185,81 @@ def merton(s0, r, sigma, mu_J, sigma_J, xi_p, T, dt=0.01, num_paths=10, reproduc
 
 	return t, S, X
 
+
+def heston(s0, r, v0, v_bar, kappa, gamma, rho, T, dt=0.01, num_paths=10,  reproducible=False):
+	"""Heston stochastic volatility model."""
+
+	# based on (8.18), (8.21), (8.3)
+	# dS(t) = r * S(t)dt + sqrt(v(t)) * S(t) * dW_x(t)
+	# dv(t) = κ(v_bar - v(t))*dt + γ*sqrt(v(t)) * [ρ*dW_x(t) + sqrt(1 - ρ^2)*dW_v(t)]
+	# dW_v*dW_x = ρ*dt
+
+	if reproducible:
+		np.random.seed(DEFAULT_SEED)
+
+	num_steps = int(T / dt)
+	Z_x = np.random.normal(0.0, 1.0, [num_paths, num_steps])
+	Z_v = np.random.normal(0.0, 1.0, [num_paths, num_steps])
+	S = np.zeros([num_paths, num_steps + 1])
+	V = np.zeros([num_paths, num_steps + 1])
+	t = np.linspace(0, T, num_steps + 1)
+
+	S[:, 0] = s0
+	V[:, 0] = v0
+
+	for i in range(num_steps):
+
+		if num_paths > 1:  # standardize samples (mean: 0, var: 1)
+			Z_x[:, i] = (Z_x[:, i] - np.mean(Z_x[:, i])) / np.std(Z_x[:, i])
+			Z_v[:, i] = (Z_v[:, i] - np.mean(Z_v[:, i])) / np.std(Z_v[:, i])
+
+		dW_x, dW_v = sqrt(dt) * Z_x[:, i], sqrt(dt) * Z_v[:, i]
+
+		S[:, i + 1] = S[:, i] + r * S[:, i] * dt + np.sqrt(V[:, i]) * S[:, i] * dW_x
+		V[:, i + 1] = V[:, i] + kappa * (v_bar - V[:, i]) * dt + gamma * np.sqrt(V[:, i]) * (rho * dW_x + sqrt(1 - rho**2) * dW_v)
+		V[:, i + 1] = np.maximum(V[:, i + 1], 0)  # truncate due to sqrt(v(t))
+
+	X = np.log(S)
+
+	return t, S, X, V
+
+
+def heston_corr(s0, r, v0, v_bar, kappa, gamma, rho, T, dt=0.01, num_paths=10, reproducible=False):
+
+	# based on (8.38)
+
+	if reproducible:
+		np.random.seed(DEFAULT_SEED)
+
+	num_steps = int(T / dt)
+	Z_x = np.random.normal(0.0, 1.0, [num_paths, num_steps])
+	Z_v = np.random.normal(0.0, 1.0, [num_paths, num_steps])
+	V = np.zeros([num_paths, num_steps + 1])
+	X = np.zeros([num_paths, num_steps + 1])
+	t = np.linspace(0, T, num_steps + 1)
+
+	X[:, 0] = log(s0)
+	V[:, 0] = v0
+
+	for i in range(num_steps):
+
+		if num_paths > 1:  # making sure that samples from normal have mean 0 and variance 1
+			Z_x[:, i] = (Z_x[:, i] - np.mean(Z_x[:, i])) / np.std(Z_x[:, i])
+			Z_v[:, i] = (Z_v[:, i] - np.mean(Z_v[:, i])) / np.std(Z_v[:, i])
+
+		Z_v[:, i] = rho * Z_x[:, i] + np.sqrt(1 - rho**2) * Z_v[:, i]  # correlate the processes
+
+		dW_x = sqrt(dt) * Z_x[:, i]
+		dW_v = sqrt(dt) * Z_v[:, i]
+		X[:, i + 1] = X[:, i] + (r - 1/2 * V[:, i]) * dt + np.sqrt(V[:, i]) * dW_x
+		V[:, i + 1] = V[:, i] + kappa * (v_bar - V[:, i]) * dt + gamma * np.sqrt(V[:, i]) * dW_v
+
+		# manual in case Z_x and Z_v are not correlated
+		# X[:, i + 1] = X[:, i] + (r - 1/2 * V[:, i]) * dt + np.sqrt(V[:, i]) * dW_x
+		# V[:, i + 1] = V[:, i] + kappa * (v_bar - V[:, i]) * dt + rho * gamma * np.sqrt(V[:, i]) * dW_x + gamma * np.sqrt((1 - rho**2) * V[:, i]) * dW_v
+
+		V[:, i + 1] = np.maximum(V[:, i + 1], 0)  # truncate due to sqrt(v(t))
+
+	S = np.exp(X)
+
+	return t, S, X, V
